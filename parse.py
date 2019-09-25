2239e105d492c80d421efb40f0a38fd0904b4e41
Abigail Fox <abigailfireflyfox@gmail.com>
2019-09-25 13:37:41 -0400

    first commit

---
+++ parse.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import csv;
+
+filename = input("enter filename to be parsed")
+
+with open(filename) as file:
+    new_filename = filename[:-3]
+    new_filename += "csv"
+    outfile = open("parsed-" + new_filename, 'a')
+    line = file.readline()
+    while(line):
+        if "Data" in line:
+            line = file.readline()
+            continue
+        if line == "\n":
+            line = file.readline()
+            continue
+        else:
+            first, second = line.split()
+            outfile.write(first)
+            outfile.write(',')
+            outfile.write(second)
+            outfile.write("\n")
+            line = file.readline()
+            
+    outfile.close()
+            
+    
